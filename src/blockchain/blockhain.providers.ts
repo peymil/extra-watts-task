4f15bb94c24d739ab050baaf7b6450eb72beb7e4
Emre Badem <emrebadem_@hotmail.com>
2022-04-12 14:33:36 +0300

    refactor: add constant tokens.

--- src/blockchain/blockhain.providers.ts
+++ src/blockchain/blockhain.providers.ts
@@ -1,10 +1,11 @@
 import { AlchemyProvider } from '@ethersproject/providers';
 import { EnvService } from 'src/types/env';
 import { ConfigService } from '@nestjs/config';
+import { ALCHEMY_CONNECTION } from './tokens';
 
 export const blockchainProviders = [
   {
-    provide: 'ALCHEMY_CONNECTION',
+    provide: ALCHEMY_CONNECTION,
     inject: [ConfigService],
     useFactory: (envVariables: EnvService) =>
       new AlchemyProvider('mainnet', envVariables.get('ALCHEMY_KEY')),
--- src/contract/contract.providers.ts
+++ src/contract/contract.providers.ts
@@ -1,11 +1,13 @@
 import { AlchemyProvider } from '@ethersproject/providers';
 import { Contract } from '@ethersproject/contracts';
 import USDT_ABI from 'src/ABIS/USDT';
+import { USDT_CONTRACT } from './tokens';
+import { ALCHEMY_CONNECTION } from 'src/blockchain/tokens';
 
 export const contractProviders = [
   {
-    provide: 'USDT_CONTRACT',
-    inject: ['ALCHEMY_CONNECTION'],
+    provide: USDT_CONTRACT,
+    inject: [ALCHEMY_CONNECTION],
     useFactory: (alchemyConnection: AlchemyProvider) =>
       new Contract(
         '0xdAC17F958D2ee523a2206206994597C13D831ec7',
