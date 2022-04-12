61313851840f1d92d857da4d56a850ec07bedded
Emre Badem <emrebadem_@hotmail.com>
2022-04-12 12:18:32 +0300

    refactor: rtransfer project to standalone app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.listen(5000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  await app.init();
 }
 bootstrap();
